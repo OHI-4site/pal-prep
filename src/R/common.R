5bad108e85958c682c6a95a30096f79243407967
saraorofino <saraorofino@bren.ucsb.edu>
2020-06-26 12:34:50 -0700

    Add us albers crs

--- src/R/common.R
+++ src/R/common.R
@@ -29,8 +29,9 @@ if (Sys.info()[['sysname']] != 'Linux' & !file.exists(dir_M)){
 }
 
 
-## standard projection for OHI global data
+## standard projections for data
 mollCRS=raster::crs('+proj=moll +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +units=m +no_defs')
+us_alb=raster::crs("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs") 
 
 ### OHI+
 
